944eac92e21353d38cc8a87732b8102df962d7cd
hariom936 <hariomverma968@gmail.com>
2022-05-13 16:24:40 +0530

    login-backends-code

---
+++ model/User-model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        min: 3,
+        max: 255
+    },
+    email:{
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    address:{
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 8,
+        max: 255
+    },
+    confirmPassword:{
+        type: String,
+        required: true,
+        min: 8,
+        max: 255
+    }
+})
+
+
+module.exports= mongoose.model('userLogin',userSchema)
